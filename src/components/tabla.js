0578ab46fbc517734ee76c3601d056c958c4c3bd
JhoanSLC <JhoanSLC4@gmail.com>
2024-05-31 20:56:52 -0500

    feat: :sparkles: Script para mandar datos a Api

---
+++ src/components/tabla.js
@@ -0,0 +1,54 @@
+import { LitElement, html, css } from "lit";
+
+export class tablaInforme extends LitElement {
+    constructor() {
+        super()
+    }
+
+    static styles = css`
+
+    table {
+        color: #fff;
+    }
+
+        td {
+            border: 1px solid rgb(204, 200, 200);
+            text-align: left;
+            padding: 8px 15px;
+        }
+    `
+
+    render() {
+        return html`
+            <table class="table table-dark" >
+                <thead >
+                    <tr>
+                        <th  scope="col">id</th>
+                        <th  scope="col">Nombre</th>
+                        <th  scope="col">Categoria</th>
+                        <th  scope="col">proveedor</th>
+                        <th  scope="col">valor unidad</th>
+                        <th  scope="col">cantidad</th>
+                        <th  scope="col">medida</th>
+                        <th  scope="col">Categoria</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Hola</td>
+                        <td>Pepito</td>
+                        <td>Perez</td>
+                        <td>asdad</td>
+                        <td>qqwe</td>
+                        <td>asd</td>
+                        <td>asd</td>
+                        <td>asd</td>
+                    </tr>
+                </tbody>
+            </table>
+        
+        `
+    }
+}
+
+customElements.define('tabla-informe', tablaInforme);
