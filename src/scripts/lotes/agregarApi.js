e952efe86ac6223506f8869ed76b24a0e74a099c
Alejo0507 <castillocazorla05@gmail.com>
2024-06-02 21:01:42 -0500

    funsion de eliminar materia prima

--- src/scripts/lotes/agregarApi.js
+++ src/scripts/lotes/agregarApi.js
@@ -1,18 +1,27 @@
-import '../mockapi/mandar'
 import { mandarMockApi } from '../mockapi/mandar';
+import { calcularGastosMaterialesLote, actualizarMateriaPrimaAPI } from '../calculos/restarMP';
 
 const URLMP = "https://66586d8e5c36170526486c75.mockapi.io/lotes";
 
 const form = document.getElementById('productForm');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  let lotes = {
-      
-      "producto": document.getElementById('producto').value,
-      "cantidad": document.getElementById('cantidadproductos').value,
-     
-  };
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-  mandarMockApi(URLMP, lotes, form);
+  // Obtener los valores del formulario
+  const producto = document.getElementById('producto').value;
+  const cantidad = parseInt(document.getElementById('cantidadproductos').value); // Convertir a número
+
+  try {
+    // Calcular los gastos de materiales para el lote de prendas
+    const gastosMaterialesLote = calcularGastosMaterialesLote([producto], cantidad);
+
+    // Actualizar los datos de materia prima en la API Mock
+    await actualizarMateriaPrimaAPI(gastosMaterialesLote);
+
+    // Mandar el lote de prendas a la API Mock
+    mandarMockApi(URLMP, { producto, cantidad }, form);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
--- src/scripts/prendasMokapi.js
+++ src/scripts/prendasMokapi.js
@@ -1,30 +1,23 @@
 
-let prendas =[
+export const prendas =[
         {
             "nombre":"Camisa manga Larga",
             "hilos":50,
+            "botones":6,
             "telas":3,
-            "sierres":0,
-            "lentejuelas":0,
-            "cauchos":0,
-            "encajes":0,
-            "costo-unidad":0,
             "id":"1"
         },
         {
             "nombre":"Camisa manga Corta",
             "hilos":45,
+            "botones":6,
             "telas":2,
-            "sierres":0,
-            "lentejuelas":0,
-            "cauchos":0,
-            "encajes":0,
-            "costo-unidad":0,
             "id":"2"
             },
         {
             "nombre":"Vestido Largo",
             "hilos":20,
+            "botones":1,
             "telas":5,
             "sierres":1,
             "lentejuelas":100,
@@ -36,6 +29,7 @@ let prendas =[
         {
             "nombre":"Vestido Corto",
             "hilos":15,
+            "botones":1,
             "telas":4,
             "sierres":1,
             "lentejuelas":50,
@@ -47,64 +41,35 @@ let prendas =[
         {
             "nombre":"Pantalon de Vestir",
             "hilos":20,
+            "botones":3,
             "telas":3,
             "sierres":1,
-            "lentejuelas":0,
+           
             "cauchos":1,
-            "encajes":0,
+            
             "costo-unidad":0,
             "id":"5"
                     },
         {
             "nombre":"Pantalon de Jean",
             "hilos":20,
+            "botones":1,
             "telas":3,
             "sierres":1,
-            "lentejuelas":0,
-            "cauchos":0,
-            "encajes":0,
+            
+           
+          
             "costo-unidad":0,
             "id":"6"
                         },
         {
             "nombre":"Short",
             "hilos":12,
+            "botones":2,
             "telas":2,
             "sierres":1,
-            "lentejuelas":0,
-            "cauchos":0,
-            "encajes":0,
-            "costo-unidad":0,
+           
             "id":"7"
                         },
 ]
 
-
-const data = JSON.stringify(prendas);
-
-// Enviar datos a la API
-fetch('https://66586d8e5c36170526486c75.mockapi.io/prendas', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: data
-})
-.then(response => {
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error('Error al enviar los datos a la API');
-    }
-})
-.then(tasks => {
-    // Almacenar la respuesta de la API en tasksObject
-    tasksObject = tasks.reduce((acc, task) => {
-        acc[task.id] = task;
-        return acc;
-    }, {});
-    console.log('Datos guardados en tasksObject:', tasksObject);
-})
-.catch(error => {
-    console.error('Ocurrió un error:', error);
-});
