0fb15cb9dc78c2fb9ec1c995aabc0043120e970c
JhoanSLC <JhoanSLC4@gmail.com>
2024-06-04 12:34:18 -0500

    feat: organización para despliegue

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import '../public/style.css';
+import '../public/boostrap.css';
+import './scripts/calculos/eficienciaOperativa';
+import './scripts/calculos/manoDeObra/thisMain';
+import './scripts/materiaPrima/agregarApi';
+import './scripts/materiaPrima/registro';
+import './scripts/productos/registro';
+import './scripts/lotes/registro';
+import './scripts/lotes/agregarApi';
+import './scripts/prendasMokapi';
+import './scripts/lotes/gastosMP';
+import './components/calculadoraEficiencia';
+import './components/manoDeObra';
+import './components/tabla';
+import './components/tablaCostos';
+import './components/tablaLotes';
+import './components/tablaObra';
+
+
+
+
+
