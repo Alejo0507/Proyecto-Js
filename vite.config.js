6e6499680aa981faf4b9f5cf632853be645fac93
JhoanSLC <JhoanSLC4@gmail.com>
2024-06-04 06:28:37 -0500

    safe build

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import { resolve } from 'path';
 
 export default defineConfig({
   server: {
-    host: true,  // Esto permite que el servidor escuche en todas las interfaces de red
-    port: 3000   // Puedes cambiar el puerto si es necesario
+    host: true, 
+    port: 3000   
   },
 
   build: {
